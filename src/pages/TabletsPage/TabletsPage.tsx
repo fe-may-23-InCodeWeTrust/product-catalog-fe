67bba4728b9ea592b0229d86fbf3ddcfe0a2eca4
alex <cebanoleksandr@gmail.com>
2023-08-28 21:30:44 +0300

    add Paginator component

--- src/pages/TabletsPage/TabletsPage.tsx
+++ src/pages/TabletsPage/TabletsPage.tsx
@@ -1,7 +1,7 @@
 import './TabletsPage.scss';
 import React from 'react';
 
-export const TabletsPage = () => {
+export const TabletsPage = () => {  
   return (
     <div>
       <h1>Tablets page</h1>
