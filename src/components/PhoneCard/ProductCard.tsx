7525c26fa936945d94ce09c51b8ce6a049d6bb09
Alyona Sarapina <alyona.sarapina@gmail.com>
2023-08-28 21:14:05 +0100

    add fonts, typography rules

--- src/components/PhoneCard/ProductCard.tsx
+++ src/components/PhoneCard/ProductCard.tsx
@@ -1,21 +1,20 @@
-import "./ProductCard.scss";
+import './ProductCard.scss';
 import React from 'react';
 
-
-export const Card: React.FC= () => {
+export const Card: React.FC = () => {
   return (
     <div className="phone-card">
-        <div
-          className="phone-card__image-container"
-        >
-          <img
-            src="https://www.vodafone.co.uk/business/media/image/1508926516174/img-1300x1000-apple-iphone-14-pro-max-deep-purple-product.png"
-            alt="phone"
-            className="phone-card__image"
-          />
-        </div>
+      <div className="phone-card__image-container">
+        <img
+          src="https://www.vodafone.co.uk/business/media/image/1508926516174/img-1300x1000-apple-iphone-14-pro-max-deep-purple-product.png"
+          alt="phone"
+          className="phone-card__image"
+        />
+      </div>
 
-        <h3 className="phone-card__title text-body">Apple iPhone 11 Pro Max 64GB Gold (iMT9G2FS/A)</h3>
+      <h3 className="phone-card__title text-body">
+        Apple iPhone 11 Pro Max 64GB Gold (iMT9G2FS/A)
+      </h3>
 
       <div className="phone-card__price-block">
         <p className="phone-card__price h3">$799</p>
@@ -40,17 +39,11 @@ export const Card: React.FC= () => {
       </div>
 
       <div className="phone-card__actions">
-        <button
-          type="submit"
-          className="addToCart text-button">
-            Add to cart
+        <button type="submit" className="addToCart text-button">
+          Add to cart
         </button>
 
-        <button
-          className="addToWishlist"
-          type="submit"
-        >
-        </button>
+        <button className="addToWishlist" type="submit"></button>
       </div>
     </div>
   );
