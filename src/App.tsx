110ca6ac44a47172fa9e71683c654bd340f75c82
AnastasiiaKhemych <anastasiia.khemych@gmail.com>
2023-08-28 14:32:54 +0300

    conected router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Footer } from './components/Footer';
+import { HomePage } from './components/HomePage';
+import { PhonesPage } from './components/PhonesPage';
+import { TabletsPage } from './components/TabletsPage';
+import { AccessoriesPage } from './components/AccessoriesPage';
 
-export const App = () => (
-  <div>
-    <h2>Product catalog</h2>
-  </div>
-);
+export const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path='home' element={<HomePage />} />
+        <Route path='phones' element={<PhonesPage />}/>
+        <Route path='tablets' element={<TabletsPage />}/>
+        <Route path='accessories' element={<AccessoriesPage />}/>
+      </Routes>
+      <Footer />
+    </div>
+  );
+};
---
+++ src/components/TabletsPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const TabletsPage = () => {
+  return (
+    <div>Tablets Page</div>
+  )
+}
