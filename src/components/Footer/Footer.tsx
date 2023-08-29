3fb55941837a349b1505a63b572330510f85bba3
AnastasiiaKhemych <anastasiia.khemych@gmail.com>
2023-08-29 15:21:52 +0300

    added footer task

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
 import logoImage from '../../images/logo.svg';
-import BackToTop from '../../images/Back to top button.svg';
 import './Footer.scss';
+import arrow from '../../assets/icons/arrow.svg';
 
 export const Footer = () => {
   return (
     <div className="footer">
-      <div>
-        <img src={logoImage} alt="logo" />
-      </div>
+      <img className="footer__logo" src={logoImage} alt="logo" />
+
       <div className="footer__nav">
-        <div className="footer__list">
-          <a href="src/components#">Github</a>
-        </div>
-        <div>Contacts</div>
-        <div>Rights</div>
+        <a className="footer__nav__item" href="src/components#">
+          Github
+        </a>
+        <a className="footer__nav__item" href="#">
+          Contacts
+        </a>
+        <a className="footer__nav__item" href="#">
+          Rights
+        </a>
       </div>
 
-      <div>
-        <button>
-          <img src={BackToTop} alt="Back to top" />
+      <div className="footer__go-back">
+        <p className="footer__go-back__label">Back to top</p>
+        <button className="footer__go-back__button">
+          <img src={arrow} alt="arrow" />
         </button>
       </div>
     </div>
