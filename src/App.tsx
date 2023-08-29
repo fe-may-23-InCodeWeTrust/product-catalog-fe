5633e6fbf92d1f28e46cb3352116a48cd0a492de
AnastasiiaKhemych <anastasiia.khemych@gmail.com>
2023-08-29 12:56:46 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,10 @@ import { Outlet } from 'react-router-dom';
 
 export const App = () => {
   return (
-  <div>
-    <h1>App</h1>
-    <Outlet></Outlet>
-    <Footer />
-  </div>
+    <div>
+      <h1>App</h1>
+      <Outlet></Outlet>
+      <Footer />
+    </div>
   );
 };
-
