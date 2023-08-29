e666342dae2a171337d5575c0432516c3e69ec2e
AnastasiiaKhemych <anastasiia.khemych@gmail.com>
2023-08-29 20:43:49 +0300

    added hover and scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Footer } from './components/Footer/Footer';
 import { Outlet } from 'react-router-dom';
+import './App.scss';
 
 export const App = () => {
   return (
-    <div>
+    <div className="app">
       <h1>App</h1>
       <Outlet></Outlet>
       <Footer />
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import logoImage from '../../images/logo.svg';
+import { Arrow } from '../../assets/icons/Arrow';
 import './Footer.scss';
-import arrow from '../../assets/icons/arrow.svg';
 
 export const Footer = () => {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
   return (
     <div className="footer">
       <img className="footer__logo" src={logoImage} alt="logo" />
 
       <div className="footer__nav">
-        <a className="footer__nav__item" href="src/components#">
+        <a
+          className="footer__nav__item"
+          href="https://github.com/fe-may-23-InCodeWeTrust"
+        >
           Github
         </a>
         <a className="footer__nav__item" href="#">
@@ -22,8 +32,8 @@ export const Footer = () => {
 
       <div className="footer__go-back">
         <p className="footer__go-back__label">Back to top</p>
-        <button className="footer__go-back__button">
-          <img src={arrow} alt="arrow" />
+        <button className="footer__go-back__button" onClick={scrollToTop}>
+          <Arrow />
         </button>
       </div>
     </div>
