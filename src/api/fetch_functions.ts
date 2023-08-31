7e6c73032c4d4c2d3e641cca3878aa3356bbd3c3
Alexey Taranenko <alloha.t.99@gmail.com>
2023-08-31 15:57:53 +0300

    temporary commit

---
+++ src/api/fetch_functions.ts
@@ -0,0 +1,6 @@
+import { Product } from '../utils/Types/Product';
+import { client } from '../utils/fetchClient';
+
+export const getProducts = (category: string, offset: string, limit: string) => {
+  return client.get<Product[]>(`/products?productType=${category}&offset=${offset}&limit=${limit}`);
+};
