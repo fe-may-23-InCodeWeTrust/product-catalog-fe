4527199d4bbcf2a1199fc0e9851fcebc0997538d
Taras Bilas <tarasbilas01@gmail.com>
2023-09-04 19:54:14 +0300

    fixed header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,6 +4,7 @@ import styles from './Header.module.scss';
 import logo from '../../assets/icons/logo.svg';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../redux/store';
+import menu from '../../assets/icons/menu.svg';
 import favourites from '../../assets/icons/favourites.svg';
 import bag from '../../assets/icons/shopping-bag.svg';
 import close from '../../assets/icons/Close.svg';
@@ -30,9 +31,9 @@ export const Header = () => {
       {!isActiveBurger && (
         <div className={styles['header']}>
           <div className={`${styles['header__left']} ${styles['left']}`}>
-            <div className={styles['left__logo']}>
+            <NavLink to="/" className={styles['left__logo']}>
               <img src={logo} alt="Nice Gadgets logo" className="logo" />
-            </div>
+            </NavLink>
 
             <nav className={`${styles['left__nav']} ${styles['nav']}`}>
               <ul className={styles['nav__list']}>
@@ -69,8 +70,17 @@ export const Header = () => {
             >
               <NavLink
                 to="favorites"
-                className={styles['header-icon__favourites-icon']}
-              ></NavLink>
+                className={({ isActive }: { isActive: boolean }) =>
+                  `${styles['header-icon__favourites-icon']} ${isActive ? styles['is-active-icon'] : ''
+                  }`
+                }
+              >
+                <img
+                  src={favourites}
+                  alt="Favourites icon"
+                  className="favourites-icon__image"
+                />
+              </NavLink>
               {!!favoritesCount && (
                 <div className={styles['mini-count']}>
                   {favoritesCount <= 9 ? favoritesCount : '9+'}
@@ -82,8 +92,17 @@ export const Header = () => {
             >
               <NavLink
                 to="cart"
-                className={styles['header-icon__shopping_bag-icon']}
-              ></NavLink>
+                className={({ isActive }: { isActive: boolean }) =>
+                  `${styles['header-icon__shopping_bag-icon']} ${isActive ? styles['is-active-icon'] : ''
+                  }`
+                }
+              >
+                <img
+                  src={bag}
+                  alt="Shopping bag icon"
+                  className="shopping_bag-icon__image"
+                />
+              </NavLink>
               {!!cartCount && (
                 <div className={styles['mini-count']}>
                   {cartCount <= 9 ? cartCount : '9+'}
@@ -96,7 +115,13 @@ export const Header = () => {
               <button
                 className={styles['header-icon__menu-icon']}
                 onClick={() => setIsActiveBurger(true)}
-              ></button>
+              >
+                <img 
+                  src={menu} 
+                  alt="Menu icon"
+                  className="menu-icon__image"
+                />
+              </button>
             </div>
           </div>
         </div>
