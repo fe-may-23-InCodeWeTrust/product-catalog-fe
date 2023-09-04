fddd771fe02a517305227f7a9f15806559ce1be1
Taras Bilas <tarasbilas01@gmail.com>
2023-09-04 12:12:39 +0300

    developed burger menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,68 +1,207 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import styles from './Header.module.scss';
 import logo from '../../assets/icons/logo.svg';
-import { NavLink } from 'react-router-dom';
+import favourites from '../../assets/icons/favourites.svg';
+import bag from '../../assets/icons/shopping-bag.svg';
+import close from '../../assets/icons/Close.svg';
 
 const getLinkClass = ({ isActive }: { isActive: boolean }) =>
   `${styles.nav__link} text-uppercase ${isActive ? styles['is-active'] : ''} `;
 
+const getBurgerMenuLinkClass = ({ isActive }: { isActive: boolean }) =>
+  `${styles.menu__link} ${isActive ? styles['is-active-menu'] : ''} `;
+
 export const Header = () => {
+  const [isActiveBurger, setIsActiveBurger] = useState(false);
+
+  useEffect(() => {
+    document.body.style.overflow = isActiveBurger ? 'hidden' : 'auto';
+  }, [isActiveBurger]);
+
   return (
-    <div className={styles['header']}>
-      <div className={`${styles['header__left']} ${styles['left']}`}>
-        <div className={styles['left__logo']}>
-          <img src={logo} alt="Nice Gadgets logo" className="logo" />
-        </div>
+    <>
+      {!isActiveBurger && (
+        <div className={styles['header']}>
+          <div className={`${styles['header__left']} ${styles['left']}`}>
+            <div className={styles['left__logo']}>
+              <img src={logo} alt="Nice Gadgets logo" className="logo" />
+            </div>
 
-        <nav className={`${styles['left__nav']} ${styles['nav']}`}>
-          <ul className={styles['nav__list']}>
-            <li className={styles['nav__item']}>
-              <NavLink to="/" className={getLinkClass}>
-                home
-              </NavLink>
-            </li>
-            <li className={styles['nav__item']}>
-              <NavLink to="phones" className={getLinkClass}>
-                phones
-              </NavLink>
-            </li>
-            <li className={styles['nav__item']}>
-              <NavLink to="tablets" className={getLinkClass}>
-                tablets
-              </NavLink>
-            </li>
-            <li className={styles['nav__item']}>
-              <NavLink to="accessories" className={getLinkClass}>
-                accessories
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-      </div>
+            <nav className={`${styles['left__nav']} ${styles['nav']}`}>
+              <ul className={styles['nav__list']}>
+                <li className={styles['nav__item']}>
+                  <NavLink to="/" className={getLinkClass}>
+                    home
+                  </NavLink>
+                </li>
 
-      <div className={`${styles['header__icons']} ${styles['icons']}`}>
-        <div
-          className={`${styles['icons__icon']} ${styles['header-icon']} ${styles['favourites-icon']}`}
-        >
-          <NavLink
-            to="favorites"
-            className={styles['header-icon__favourites-icon']}
-          ></NavLink>
+                <li className={styles['nav__item']}>
+                  <NavLink
+                    to="phones"
+                    className={getLinkClass}
+                  >
+                    phones
+                  </NavLink>
+                </li>
+
+                <li className={styles['nav__item']}>
+                  <NavLink
+                    to="tablets"
+                    className={getLinkClass}
+                  >
+                    tablets
+                  </NavLink>
+                </li>
+
+                <li className={styles['nav__item']}>
+                  <NavLink
+                    to="accessories"
+                    className={getLinkClass}
+                  >
+                    accessories
+                  </NavLink>
+                </li>
+              </ul>
+            </nav>
+          </div>
+
+          <div className={`${styles['header__icons']} ${styles['icons']}`}>
+            <div
+              className={`${styles['icons__icon']} ${styles['header-icon']} ${styles['favourites-icon']}`}
+            >
+              <NavLink
+                to="favorites"
+                className={styles['header-icon__favourites-icon']}
+              ></NavLink>
+            </div>
+            <div
+              className={`${styles['icons__icon']} ${styles['header-icon']} ${styles['shopping_bag-icon']}`}
+            >
+              <NavLink
+                to="cart"
+                className={styles['header-icon__shopping_bag-icon']}
+              ></NavLink>
+            </div>
+            <div
+              className={`${styles['icons__icon']} ${styles['header-icon']} ${styles['menu-icon']}`}
+            >
+              <button className={styles['header-icon__menu-icon']} onClick={() => setIsActiveBurger(true)}></button>
+            </div>
+          </div>
         </div>
-        <div
-          className={`${styles['icons__icon']} ${styles['header-icon']} ${styles['shopping_bag-icon']}`}
-        >
-          <NavLink
-            to="cart"
-            className={styles['header-icon__shopping_bag-icon']}
-          ></NavLink>
-        </div>
-        <div
-          className={`${styles['icons__icon']} ${styles['header-icon']} ${styles['menu-icon']}`}
-        >
-          <a href="#" className={styles['header-icon__menu-icon']}></a>
-        </div>
-      </div>
-    </div>
+      )}
+
+      {isActiveBurger && (
+        <aside className={`${styles['aside']}`}>
+          <div className={`${styles['aside__top']}`}>
+            <NavLink
+              to="/"
+              className={`${styles['aside__logo']} ${styles['logo']}`}
+              onClick={() => setIsActiveBurger(false)}
+            >
+              <img
+                src={logo}
+                alt="Nice Gadgets logo"
+                className={`${styles['logo__image']}`}
+              />
+            </NavLink>
+
+            <div className={`${styles['aside__close-icon']} ${styles['close-icon']}`}>
+              <NavLink
+                to="/"
+                className={`${styles['close-icon__link']}`}
+                onClick={() => setIsActiveBurger(false)}
+              >
+                <img
+                  src={close}
+                  alt="Close icon"
+                  className={`${styles['close-icon__image']}`}
+                />
+              </NavLink>
+            </div>
+          </div>
+
+          <div className={`${styles['aside__burger-menu']} ${styles['burger-menu']}`}>
+            <nav className={`${styles['burger-menu__nav']}`}>
+              <ul className={`${styles['burger-menu__list']}`}>
+                <li className={`${styles['burger-menu__item']}`}>
+                  <NavLink
+                    to="/"
+                    className={getBurgerMenuLinkClass}
+                    onClick={() => setIsActiveBurger(false)}
+                  >
+                    home
+                  </NavLink>
+                </li>
+
+                <li className={`${styles['burger-menu__item']}`}>
+                  <NavLink
+                    to="phones"
+                    className={getBurgerMenuLinkClass}
+                    onClick={() => setIsActiveBurger(false)}
+                  >
+                    phones
+                  </NavLink>
+                </li>
+
+                <li className={`${styles['burger-menu__item']}`}>
+                  <NavLink
+                    to="tablets"
+                    className={getBurgerMenuLinkClass}
+                    onClick={() => setIsActiveBurger(false)}
+                  >
+                    tablets
+                  </NavLink>
+                </li>
+
+                <li className={`${styles['burger-menu__item']}`}>
+                  <NavLink
+                    to="accessories"
+                    className={getBurgerMenuLinkClass}
+                    onClick={() => setIsActiveBurger(false)}
+                  >
+                    accessories
+                  </NavLink>
+                </li>
+              </ul>
+            </nav>
+          </div>
+
+          <div className={`${styles['aside__bottom']}`}>
+            <div className={`${styles['aside__favourites-icon']} ${styles['favourites-icon']}`}>
+              <NavLink
+                to="favorites"
+                className={({ isActive }: { isActive: boolean }) =>
+                  `${styles['favourites-icon__link']} ${isActive ? styles['is-active-icon'] : ''}`
+                }
+                onClick={() => setIsActiveBurger(false)}
+              >
+                <img
+                  src={favourites}
+                  alt="Favourites icon"
+                  className={`${styles['favourites-icon__image']}`}
+                />
+              </NavLink>
+            </div>
+            <div className={`${styles['aside__shopping_bag-icon']} ${styles['shopping_bag-icon']}`}>
+              <NavLink
+                to="cart"
+                className={({ isActive }: { isActive: boolean }) =>
+                  `${styles['shopping_bag-icon__link']} ${isActive ? styles['is-active-icon'] : ''}`
+                }
+                onClick={() => setIsActiveBurger(false)}
+              >
+                <img
+                  src={bag}
+                  alt="Shopping bag icon"
+                  className={`${styles['shopping_bag-icon__image']}`}
+                />
+              </NavLink>
+            </div>
+          </div>
+        </aside>
+      )}
+    </>
   );
 };
