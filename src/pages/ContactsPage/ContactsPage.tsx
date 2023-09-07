aa09e9da3d1ac44ce985f2decea45fde11dcbf7f
alex <cebanoleksandr@gmail.com>
2023-09-07 14:24:46 +0300

    add icons to the contacts

--- src/pages/ContactsPage/ContactsPage.tsx
+++ src/pages/ContactsPage/ContactsPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './ContactsPage.module.scss';
 import { contacts } from '../../utils/contacts';
 import { BackButton } from '../../components/BackButton/BackButton';
@@ -6,6 +6,13 @@ import { useTranslation } from 'react-i18next';
 
 export const ContactsPage = () => {
   const { t } = useTranslation();
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  }, [])
 
   return (
     <div className={styles['contacts_container']}>
@@ -27,15 +34,36 @@ export const ContactsPage = () => {
             <p className={styles['contact_name']}>{contact.fullName}</p>
 
             <p className={styles['contact_info']}>
-              <a href={contact.linkedIn}>LinkedIn</a>
+              <a href={contact.linkedIn}>
+                <img
+                  src="https://static-00.iconduck.com/assets.00/linkedin-icon-2048x2048-ya5g47j2.png"
+                  className={styles['contact_icon']}
+                  alt="LinkedIn"
+                />
+                LinkedIn
+              </a>
             </p>
 
             <p className={styles['contact_info']}>
-              <a href={contact.gitHub}>Github</a>
+              <a href={contact.gitHub}>
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+                  className={styles['contact_icon']}
+                  alt="Github"
+                />
+                Github
+              </a>
             </p>
 
             <p className={styles['contact_info']}>
-              <a href={`mailto:${contact.email}`}>Email</a>
+              <a href={`mailto:${contact.email}`}>
+                <img
+                  src="https://cdn4.iconfinder.com/data/icons/social-media-logos-6/512/112-gmail_email_mail-512.png"
+                  className={styles['contact_icon']}
+                  alt="Email"
+                />
+                Email
+              </a>
             </p>
           </div>
         ))}
